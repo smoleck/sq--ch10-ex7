707abd03129fb5377478f8c6a244cadd2d0dfd02
smoleck <kacper.smolkowski@gmail.com>
2025-07-18 15:45:43 +0200

    init

---
+++ src/main/java/com/example/sq_/ch10_ex7/SqCh10Ex7Application.java
@@ -0,0 +1,13 @@
+package com.example.sq_.ch10_ex7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqCh10Ex7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqCh10Ex7Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sq_/ch10_ex7/SqCh10Ex7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sq_.ch10_ex7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqCh10Ex7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
